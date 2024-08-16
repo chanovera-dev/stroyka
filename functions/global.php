ad81ba8d612e972ab1d9341acfd40a0ba16f9cd0
Chano Vera <github@chanovera.com>
2024-08-16 07:39:51 -0600

    support for submenus for two levels in mobile

--- functions/global.php
+++ functions/global.php
@@ -63,6 +63,8 @@ function stroyka_theme_custom_global() {
                 .menu-mobile::-webkit-scrollbar{display:none;}
                 #menu-mobile ul{padding:0;}
                 #menu-mobile ul li a{padding:0 20px;display:flex;align-items:center;height:48px;color:var(--wp--preset--color--text);border-bottom:1px solid var(--wp--preset--color--line);border-radius:0;}
+                #menu-mobile ul li ul li a{padding:30px;}
+                #menu-mobile ul li ul li ul li a{padding:40px;}
                 /* submenús */
                 #menu-mobile > ul li.menu-item-has-children{position:relative;}
                     /* botón */

14c09fc95e5cf3b7c695e3b1dd63c2dc4261b6fe
Chano Vera <github@chanovera.com>
2024-08-16 22:43:11 -0600

    vsible hero

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 
     echo '
     <main id="main">';
-        // include(TEMPLATEPATH . '/parts/frontpage/hero.php');
+        include(TEMPLATEPATH . '/parts/frontpage/hero.php');
         // include(TEMPLATEPATH . '/parts/frontpage/features.php');
         // include(TEMPLATEPATH . '/parts/frontpage/featured-products.php');
         // include(TEMPLATEPATH . '/parts/frontpage/catalog.php');

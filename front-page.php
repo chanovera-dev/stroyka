8f1e375518169f8b3a19a26be823627d60a6f246
Chano Vera <github@chanovera.com>
2024-08-16 23:08:38 -0600

    show features

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
     echo '
     <main id="main">';
         include(TEMPLATEPATH . '/parts/frontpage/hero.php');
-        // include(TEMPLATEPATH . '/parts/frontpage/features.php');
+        include(TEMPLATEPATH . '/parts/frontpage/features.php');
         // include(TEMPLATEPATH . '/parts/frontpage/featured-products.php');
         // include(TEMPLATEPATH . '/parts/frontpage/catalog.php');
         // include(TEMPLATEPATH . '/parts/frontpage/bestsellers.php');

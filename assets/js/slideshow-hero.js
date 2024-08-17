ced9d3b7ddc0decb8b7f1570e67fd0510582903f
Chano Vera <github@chanovera.com>
2024-08-16 23:00:49 -0600

    hero touchy

--- assets/js/slideshow-hero.js
+++ assets/js/slideshow-hero.js
@@ -8,28 +8,12 @@ function slideshowHero() {
     const card3 = document.querySelector('.slide3');
 
     let startX = 0;
-    let currentX = 0;
+    let isSwiping = false;
 
     function left0() {slideshow.style.transform = "translateX(0)";}
     function left33() {slideshow.style.transform = "translateX(-33.3333%)";}
     function left66() {slideshow.style.transform = "translateX(-66.6666%)";}
 
-    card1.classList.add('active');
-
-    button1.addEventListener('click', function(){
-        left0();
-        setActiveSlide(1);
-    });
-    button2.addEventListener('click', function(){
-        left33();
-        setActiveSlide(2);
-    });
-    button3.addEventListener('click', function(){
-        left66();
-        setActiveSlide(3);
-    });
-
-    // Función para establecer la diapositiva activa
     function setActiveSlide(slideNumber) {
         button1.classList.toggle('active', slideNumber === 1);
         button2.classList.toggle('active', slideNumber === 2);
@@ -39,36 +23,58 @@ function slideshowHero() {
         card3.classList.toggle('active', slideNumber === 3);
     }
 
+    button1.addEventListener('click', function(){
+        left0();
+        setActiveSlide(1);
+    });
+
+    button2.addEventListener('click', function(){
+        left33();
+        setActiveSlide(2);
+    });
+
+    button3.addEventListener('click', function(){
+        left66();
+        setActiveSlide(3);
+    });
+
     // Eventos táctiles
     slideshow.addEventListener('touchstart', function(e) {
         startX = e.touches[0].clientX;
+        isSwiping = true;
     });
 
     slideshow.addEventListener('touchmove', function(e) {
-        currentX = e.touches[0].clientX;
+        if (isSwiping) {
+            const currentX = e.touches[0].clientX;
+            const deltaX = startX - currentX;
+
+            if (deltaX > 50) {
+                // Deslizar hacia la izquierda
+                isSwiping = false;
+                if (button1.classList.contains('active')) {
+                    left33();
+                    setActiveSlide(2);
+                } else if (button2.classList.contains('active')) {
+                    left66();
+                    setActiveSlide(3);
+                }
+            } else if (deltaX < -50) {
+                // Deslizar hacia la derecha
+                isSwiping = false;
+                if (button3.classList.contains('active')) {
+                    left33();
+                    setActiveSlide(2);
+                } else if (button2.classList.contains('active')) {
+                    left0();
+                    setActiveSlide(1);
+                }
+            }
+        }
     });
 
     slideshow.addEventListener('touchend', function() {
-        const deltaX = startX - currentX;
-        if (deltaX > 50) {
-            // Deslizar hacia la izquierda
-            if (button1.classList.contains('active')) {
-                left33();
-                setActiveSlide(2);
-            } else if (button2.classList.contains('active')) {
-                left66();
-                setActiveSlide(3);
-            }
-        } else if (deltaX < -50) {
-            // Deslizar hacia la derecha
-            if (button3.classList.contains('active')) {
-                left33();
-                setActiveSlide(2);
-            } else if (button2.classList.contains('active')) {
-                left0();
-                setActiveSlide(1);
-            }
-        }
+        isSwiping = false;
     });
 }
 

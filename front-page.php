12fb2ba2d942d1f85e4603dbc2a9fb0e6128eddb
Chano Vera <github@chanovera.com>
2024-08-18 11:03:59 -0600

    show featured products in front-page

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
     <main id="main">';
         include(TEMPLATEPATH . '/parts/frontpage/hero.php');
         include(TEMPLATEPATH . '/parts/frontpage/features.php');
-        // include(TEMPLATEPATH . '/parts/frontpage/featured-products.php');
+        include(TEMPLATEPATH . '/parts/frontpage/featured-products.php');
         // include(TEMPLATEPATH . '/parts/frontpage/catalog.php');
         // include(TEMPLATEPATH . '/parts/frontpage/bestsellers.php');
         // include(TEMPLATEPATH . '/parts/frontpage/categories.php');

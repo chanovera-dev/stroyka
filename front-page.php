8006bc609f98156b9cb98732a75054b199c371ab
Chano Vera <github@chanovera.com>
2024-08-18 07:15:55 -0600

    correction

--- front-page.php
+++ front-page.php
@@ -5,8 +5,8 @@
     <main id="main">';
         include(TEMPLATEPATH . '/parts/frontpage/hero.php');
         include(TEMPLATEPATH . '/parts/frontpage/features.php');
-        include(TEMPLATEPATH . '/parts/frontpage/featured-products.php');
-        include(TEMPLATEPATH . '/parts/frontpage/catalog.php');
+        // include(TEMPLATEPATH . '/parts/frontpage/featured-products.php');
+        // include(TEMPLATEPATH . '/parts/frontpage/catalog.php');
         // include(TEMPLATEPATH . '/parts/frontpage/bestsellers.php');
         // include(TEMPLATEPATH . '/parts/frontpage/categories.php');
         // include(TEMPLATEPATH . '/parts/frontpage/arrivals.php');
